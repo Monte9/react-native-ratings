f70bfa62075b9888bf2bd08ae81313204864695a
Simen Bekkhus <sbekkhus91@gmail.com>
2019-08-27 15:23:36 +0200

    fix(ts): add missing import

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ImageURISource, ViewPropTypes } from 'react-native';
+import { ImageStyle, ImageURISource, ViewPropTypes } from 'react-native';
 
 export interface RatingProps {
 
