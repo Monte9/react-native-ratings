f3fdb5dbb3e7098bf3f5367681c43fbe11a28c01
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-04-12 00:21:18 +0530

    added scripts and removed index.d.ts

--- src/SwipeRating.tsx
+++ src/SwipeRating.tsx
@@ -61,24 +61,121 @@ const fractionsType: any = ( props, propName, componentName ) => {
 };
 
 export type SwipeRatingProps = {
+
+  /**
+   * Graphic used for represent a rating
+   *
+   * Default is 'star'
+   */
   type?: string;
+
+  /**
+   * Pass in a custom image source; use this along with type='custom' prop above
+   */
   ratingImage?: React.ReactNode;
+
+  /**
+   * Pass in a custom fill-color for the rating icon; use this along with type='custom' prop above
+   *
+   * Default is '#f1c40f'
+   */
   ratingColor?: string;
+
+  /**
+   * Pass in a custom background-fill-color for the rating icon; use this along with type='custom' prop above
+   *
+   * Default is 'white'
+   */
   ratingBackgroundColor?: string;
+
+  /**
+   * Number of rating images to display
+   *
+   * Default is 5
+   */
   ratingCount?: number;
+
+  /**
+   * Color used for the text labels
+   */
   ratingTextColor?: string;
+
+  /**
+   * The size of each rating image
+   *
+   * Default is 50
+   */
   imageSize?: number;
+
+  /**
+   * Callback method when the user starts rating.
+   */
   onStartRating?: Function;
+
+  /**
+   * Callback method when the user finishes rating. Gives you the final rating value as a whole number
+   */
   onFinishRating?: Function;
+
+  /**
+   * Displays the Built-in Rating UI to show the rating value in real-time
+   *
+   * Default is false
+   */
   showRating?: boolean;
+
+  /**
+   * Exposes style prop to add additonal styling to the container view
+   */
   style?: Object;
+
+  /**
+   * Whether the rating can be modiefied by the user
+   *
+   * Default is false
+   */
   readonly?: boolean;
+
+  /**
+   * Whether the text is read only
+   *
+   * Default is false
+   */
   showReadOnlyText?: boolean;
+
+  /**
+   * The initial rating to render
+   *
+   * Default is ratingCount/2
+   */
   startingValue?: number;
+
+  /**
+   * The number of decimal places for the rating value; must be between 0 and 20
+   */
   fractions?: typeof fractionsType;
+
+  /**
+   * The minimum value the user can select
+   *
+   * Default is 0
+   */
   minValue?: number;
+
+  /**
+   * Callback method when the user is swiping.
+   */
   onSwipeRating?: ( number ) => void;
+
+  /**
+   * Color used for the background
+   */
   tintColor?: string;
+
+  /**
+   * The number to jump per swipe
+   * Default is 0 (not to jump)
+   */
   jumpValue?: number;
 };
 
--- src/TapRating.tsx
+++ src/TapRating.tsx
@@ -7,17 +7,87 @@ import { StyleSheet, Text, View, StyleProp, ViewStyle } from "react-native";
 import Star from "./components/Star";
 
 export type TapRatingProps = {
+
+  /**
+   * Total number of ratings to display
+   *
+   * Default is 5
+   */
   count?: number;
+
+  /**
+   * Labels to show when each value is tapped
+   *
+   * e.g. If the first star is tapped, then value in index 0 will be used as the label
+   *
+   * Default is ['Terrible', 'Bad', 'Okay', 'Good', 'Great']
+   */
   reviews?: string[];
+
+  /**
+   * Determines if to show the reviews above the rating
+   *
+   * Default is true
+   */
   showRating?: boolean;
+
+  /**
+   * Color value for review.
+   *
+   * Default is #f1c40f
+   */
   reviewColor?: string;
+
+  /**
+   * Size value for review.
+   *
+   * Default is 40
+   */
   reviewSize?: number;
+
+  /**
+   * Initial value for the rating
+   *
+   * Default is 3
+   */
   defaultRating?: number;
+
+  /**
+   * Style for star container
+   *
+   * Default is none
+   */
   starContainerStyle?: StyleProp<ViewStyle>;
+
+  /**
+   * Callback method when the user finishes rating. Gives you the final rating value as a whole number
+   */
   onFinishRating?: ( number ) => void;
+
+  /**
+   * Whether the rating can be modiefied by the user
+   *
+   * Default is false
+   */
   isDisabled?: boolean;
+
+  /**
+   * Color value for filled stars.
+   *
+   * Default is #004666
+   */
   selectedColor?: string;
+
+  /**
+   * Size of rating image
+   *
+   * Default is 40
+   */
   size?: number;
+
+  /**
+   * Pass in a custom base image source
+   */
   starImage?: string;
 };
 
